461a909b3f78c11a44a77fbbb7d23bde2096f4c5
Renato Moreno <renats.mc@gmail.com>
2022-08-25 13:40:14 -0300

    adiciona javascript + firebase

---
+++ semana_front_end_mundo_invertido/assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7r4GsPtrG3CwRzu1iIxBoM5QkslYTniE",
+  authDomain: "projeto-teste-40579.firebaseapp.com",
+  projectId: "projeto-teste-40579",
+  storageBucket: "projeto-teste-40579.appspot.com",
+  messagingSenderId: "682366746009",
+  appId: "1:682366746009:web:c4c85e3ea7abb8a4f2a708",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
